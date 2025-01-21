f8556147cc3a91dcb6b7b1bb543995399d0d7d0a
hamidou123456 <142541060+hamidou123456@users.noreply.github.com>
2025-01-21 11:50:36 +0100

    update flask

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(video.ai)
+app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-if video.ai == '__main__':
+if __name__ == '__main__':
     app.run(debug=True)
