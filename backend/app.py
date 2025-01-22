4f851aff6aefb3ac8bb0d50a3fb6180081f7294b
fatmatroudi <fatmatroudi6@gmail.com>
2025-01-22 13:27:28 +0100

    add video api

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
+
+def generate_video(prompt):
+    # generate video
+    return 'mov_bbb.mp4'
+
+
+
+@app.route('/api/video', methods=['POST'])
 def hello_world():
-    return 'Hello, World!'
+    if not request.json or 'prompt' not in request.json:
+        return jsonify({"error": "Prompt is required"}), 400
+    
+    prompt = request.json['prompt']
+    
+    # generate video
+    video_path = generate_video(prompt)
+
+    video_data = {
+        'prompt': prompt,
+        'video_path': video_path
+    }
+
+    return jsonify(video_data), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
